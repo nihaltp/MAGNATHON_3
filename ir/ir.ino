437db495e2a3d086d82134c52a4ac189a3054f40
NIHAL  T P <nihaltpnki@gmail.com>
2026-02-10 02:46:08 +0530

    Add display

--- ir/ir.ino
+++ ir/ir.ino
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <LedControl.h>
 
-#define IR1 13
+#define SEGMENT_LOAD_PIN 7
+#define SEGMENT_CLOCK_PIN 13
+#define SEGMENT_DATA_PIN 11
+
+#define IR1 9
 #define IR2 12
-#define IR3 11
+#define IR3 8
 #define IR4 10
+
+LedControl lc = LedControl(SEGMENT_DATA_PIN, SEGMENT_CLOCK_PIN, SEGMENT_LOAD_PIN);
 
 int score = 0;
 
@@ -14,6 +21,9 @@ bool pause = false;
 bool failed = false;
 
 bool checkIR();
+void displaySetup();
+void showNumber(int number);
+void displayScore();
 
 void setup() {
   Serial.begin(9600);
@@ -22,6 +32,8 @@ void setup() {
   pinMode(IR2, INPUT);
   pinMode(IR3, INPUT);
   pinMode(IR4, INPUT);
+
+  displaySetup();
 }
 
 void loop() {
@@ -82,4 +94,20 @@ void loop() {
 
 bool checkIR() {
   return digitalRead(IR1) && digitalRead(IR2) && digitalRead(IR3) && digitalRead(IR4);
+}
+
+void displaySetup() {
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 8);
+  lc.clearDisplay(0);
+}
+
+void showNumber(int number) {
+  lc.setDigit(0, 0, number % 10, false);         // Units
+  lc.setDigit(0, 1, (number / 10) % 10, false);  // Tens
+  lc.setDigit(0, 2, number / 100, false);        // Hundreds
+}
+
+void displayScore() {
+  showNumber(score);
 }
